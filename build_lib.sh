eb00f33272adad508085ddf869ad8547e3ea0e76
Rex Fernando <rex.fernando@aptoslabs.com>
2024-03-01 20:59:07 -0800

    macos build logic

--- build_lib.sh
+++ build_lib.sh
@@ -5,10 +5,18 @@ set -e
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
-num_cpus=`grep -c ^processor /proc/cpuinfo`
-./build_gmp.sh host || true
-mkdir -p build_prover
-cd build_prover
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package ..
-make -j$num_cpus 
-make install
+if [[ $(uname -s) == "Darwin" ]]; then
+  ./build_gmp.sh macos_arm64 || true
+  mkdir build_prover_macos_arm64 && cd build_prover_macos_arm64
+  cmake .. -DTARGET_PLATFORM=macos_arm64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package_macos_arm64
+  make -j4 && make install
+else
+  num_cpus=`grep -c ^processor /proc/cpuinfo`
+  ./build_gmp.sh host || true
+  mkdir -p build_prover
+  cd build_prover
+  cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package ..
+  make -j$num_cpus 
+  make install
+fi
+
