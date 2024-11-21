bf7c3d09db3eea1321cebdcb45f9a93c2e84cfa0
nathan.qin <nathan.qin@cmexpro.com>
2024-11-21 15:30:04 +0800

    --wip-- [skip ci]

--- build_lib.sh
+++ build_lib.sh
@@ -6,16 +6,15 @@ export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
 if [[ $(uname -s) == "Darwin" ]]; then
-  ./build_gmp.sh macos_arm64
-  mkdir -p build_prover_macos_arm64 
-  cd build_prover_macos_arm64
-  cmake .. -DTARGET_PLATFORM=macos_arm64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package
-  make -j4 && make install
+    ./build_gmp.sh macos_arm64
+    mkdir -p build_prover_macos_arm64
+    cd build_prover_macos_arm64
+    cmake .. --preset=default -DTARGET_PLATFORM=macos_arm64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package
+    make -j4 && make install
 else
-  mkdir -p build_prover
-  cd build_prover
-  cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package ..
-  make -j$(nproc)
-  make install
+    mkdir -p build_prover
+    cd build_prover
+    cmake .. -LAH -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package ..
+    make -j$(nproc)
+    make install
 fi
-
